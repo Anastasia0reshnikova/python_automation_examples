6af9405775cd2fa469770d1166696cec1698eee6
anastasiia_oreshnikova <a_oreshnikova@apple.com>
2025-09-01 11:25:16 +0200

    added LoginPage

--- ui/demo_pages.py
+++ ui/demo_pages.py
@@ -1,16 +1,35 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class LoginPage:
+    """Page Object для страницы логина"""
 
-    def __init__(self, d, base):
-        self.d, self.base = d, base
+    # локаторы
+    USERNAME_INPUT = (By.ID, "userName")
+    PASSWORD_INPUT = (By.ID, "password")
+    LOGIN_BTN = (By.ID, "login")
+
+    def __init__(self, driver, base_url, timeout: int = 5):
+        self.driver = driver
+        self.base_url = base_url
+        self.wait = WebDriverWait(driver, timeout)
 
     def open(self):
-        self.d.get(f"{self.base}/login")
+        """Открыть страницу логина"""
+        self.driver.get(f"{self.base_url}/login")
+        # убедиться, что форма загрузилась
+        self.wait.until(EC.visibility_of_element_located(self.USERNAME_INPUT))
+        return self
 
-    def sign_in(self, user, pwd):
-        self.d.find_element(By.ID,"userName").send_keys(user)
-        self.d.find_element(By.ID,"password").send_keys(pwd)
-        self.d.find_element(By.ID,"login").click()
-
+    def sign_in(self, username: str, password: str):
+        """Авторизоваться пользователем"""
+        self.driver.find_element(*self.USERNAME_INPUT).send_keys(username)
+        self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
+        self.driver.find_element(*self.LOGIN_BTN).click()
+        # ждём, пока исчезнет кнопка логина (или появится другой элемент)
+        self.wait.until(EC.invisibility_of_element_located(self.LOGIN_BTN))
+        # возвращаем новую страницу, например HomePage
+        # return HomePage(self.driver, self.base_url)
+        return self
